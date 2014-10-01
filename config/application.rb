721c827afda0fc721c99097a9cc51745e82022b5
Michael <salamandermike@gmail.com>
2014-10-01 10:23:27 -0700

    preparing for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module HalloweenHangman
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
     # We don't want the default of everything that isn't js or css, because it pulls too many things in
