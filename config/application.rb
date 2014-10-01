73074f8f28884fc88a2e33b369d5d89a877eb9e4
Michael <salamandermike@gmail.com>
2014-10-01 12:20:20 -0700

    tweeking deployment assets

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ Bundler.require(*Rails.groups)
 
 module HalloweenHangman
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("vendor", "assets", "components")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -26,20 +28,5 @@ module HalloweenHangman
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
-
-    # We don't want the default of everything that isn't js or css, because it pulls too many things in
-    config.assets.precompile.shift
-
-    # Explicitly register the extensions we are interested in compiling
-    config.assets.precompile.push(Proc.new do |path|
-      File.extname(path).in? [
-        '.html', '.erb', '.haml',                 # Templates
-        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
-        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-      ]
-    end)
   end
 end
