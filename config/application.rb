11ebcb28aa9a5f975ff593bf1a74ddfb86dc92ed
Michael <salamandermike@gmail.com>
2014-10-01 10:55:20 -0700

    tweeking deployment assets

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module HalloweenHangman
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # config.serve_static_assets = true
+
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
     # We don't want the default of everything that isn't js or css, because it pulls too many things in
