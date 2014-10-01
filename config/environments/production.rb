19ca53c8511348e70987afbe7a9b66e3a4be3b3c
Michael <salamandermike@gmail.com>
2014-10-01 12:31:04 -0700

    tweeking deployment assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
-  config.assets.precompile += %w( halloween.jpg )
+  # config.assets.precompile += %w( halloween.jpg )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
