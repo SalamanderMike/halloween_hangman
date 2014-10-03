2d35b8818433c02344633acdfc092f52ba45e12a
Michael <salamandermike@gmail.com>
2014-10-03 11:30:58 -0700

    tweeking deployment assets

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module HalloweenHangman
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("vendor", "assets", "components")
 
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
